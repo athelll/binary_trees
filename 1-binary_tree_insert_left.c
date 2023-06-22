22e83b4400cf5acec6e0634aef58a724755c3c3c
6felix <felix.okoronkwo.1@gmail.com>
2023-06-22 21:01:57 +0100

    Function inserts new left-node to parent node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
+	if (!node || !parent)
+		return (NULL);
+
 	if (parent->left)
 	{
 		node->left = parent->left;
